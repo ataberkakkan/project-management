7c982dedbf7e2cbb000052e0124933173b3b1241
ataberkakkan <akkanataberk@gmail.com>
2024-09-30 14:26:36 +0300

    Fixed a wrong auto import

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,3 @@
-import { getUserTasks } from "./../../../server/src/controllers/taskController";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export interface Project {
